1f5112c00df8f0d481ff2891718cf30fa80873c6
Freddy Remache <fremache3525@uta.edu.ec>
2023-03-19 00:11:43 -0500

    Controladores 0:11

---
+++ ApiPaginaWeb/Controllers/DetallesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiPaginaWeb.Models;
+
+namespace ApiPaginaWeb.Controllers
+{
+    public class DetallesController : ApiController
+    {
+        private ComprasEntities db = new ComprasEntities();
+
+        // GET: api/Detalles
+        public IQueryable<Detalles> GetDetalles()
+        {
+            return db.Detalles;
+        }
+
+        // GET: api/Detalles/5
+        [ResponseType(typeof(Detalles))]
+        public IHttpActionResult GetDetalles(int id)
+        {
+            Detalles detalles = db.Detalles.Find(id);
+            if (detalles == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(detalles);
+        }
+
+        // PUT: api/Detalles/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDetalles(int id, Detalles detalles)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != detalles.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(detalles).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetallesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Detalles
+        [ResponseType(typeof(Detalles))]
+        public IHttpActionResult PostDetalles(Detalles detalles)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Detalles.Add(detalles);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = detalles.id }, detalles);
+        }
+
+        // DELETE: api/Detalles/5
+        [ResponseType(typeof(Detalles))]
+        public IHttpActionResult DeleteDetalles(int id)
+        {
+            Detalles detalles = db.Detalles.Find(id);
+            if (detalles == null)
+            {
+                return NotFound();
+            }
+
+            db.Detalles.Remove(detalles);
+            db.SaveChanges();
+
+            return Ok(detalles);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DetallesExists(int id)
+        {
+            return db.Detalles.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ ApiPaginaWeb/Controllers/FacturasController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiPaginaWeb.Models;
+
+namespace ApiPaginaWeb.Controllers
+{
+    public class FacturasController : ApiController
+    {
+        private ComprasEntities db = new ComprasEntities();
+
+        // GET: api/Facturas
+        public IQueryable<Facturas> GetFacturas()
+        {
+            return db.Facturas;
+        }
+
+        // GET: api/Facturas/5
+        [ResponseType(typeof(Facturas))]
+        public IHttpActionResult GetFacturas(int id)
+        {
+            Facturas facturas = db.Facturas.Find(id);
+            if (facturas == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(facturas);
+        }
+
+        // PUT: api/Facturas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFacturas(int id, Facturas facturas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != facturas.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(facturas).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacturasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Facturas
+        [ResponseType(typeof(Facturas))]
+        public IHttpActionResult PostFacturas(Facturas facturas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Facturas.Add(facturas);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = facturas.id }, facturas);
+        }
+
+        // DELETE: api/Facturas/5
+        [ResponseType(typeof(Facturas))]
+        public IHttpActionResult DeleteFacturas(int id)
+        {
+            Facturas facturas = db.Facturas.Find(id);
+            if (facturas == null)
+            {
+                return NotFound();
+            }
+
+            db.Facturas.Remove(facturas);
+            db.SaveChanges();
+
+            return Ok(facturas);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FacturasExists(int id)
+        {
+            return db.Facturas.Count(e => e.id == id) > 0;
+        }
+    }
+}
